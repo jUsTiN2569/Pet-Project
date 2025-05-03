05c8d7ae1b2173bc5c66039e5c12b4dfd96d1fe7
jUsTiN2569 <justingu2020@gmail.com>
2025-05-02 17:54:58 -0700

    update

---
+++ Pet Tracker/pet.py
@@ -0,0 +1,13 @@
+class pet:
+    def __init__(self, hunger, happiness, energy):
+
+        self.hunger = hunger
+        self.happiness = happiness
+        self.energy = energy
+
+    
+
+        
+        
+
+    
