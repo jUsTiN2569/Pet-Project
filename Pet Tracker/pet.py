5b583801caea2b47ac054be912cbe3d2771dbaa1
jUsTiN2569 <justingu2020@gmail.com>
2025-05-02 17:56:14 -0700

    Pet Tracker

--- Pet Tracker/pet.py
+++ Pet Tracker/pet.py
@@ -5,7 +5,33 @@ class pet:
         self.happiness = happiness
         self.energy = energy
 
-    
+    def Pet_Tracker(self):
+        print("\nHi, welcome to my pet tracker app.")
+        name = input("What is your pet's name? ")
+        while True:
+            print("[Options: feed, play, rest, status, exit]")
+            ask = input("What would you like to do? ")
+            if ask == "feed":
+                print(f"\nFed {name}! Hunger decreased.\n")
+
+            elif ask == "play":
+                print(f"Played with {name}! Happiness increased, energy decreased.\n")
+
+            elif ask == "rest":
+                print(f"{name} has slept soundly! Energy increased.\n")
+
+            elif ask == "status":
+                print(f"{name}'s Status: Hunger: {self.hunger}\nHappiness: {self.happiness}\nEnergy: {self.energy}")
+
+            elif ask == "update":
+                print(f"Updating {name}'s status, time is going by! Your pet is getting tired and hungry!\n")
+
+            else:
+                print("You may have typed something wrong, please retype!\n")
+            
+        
+if __name__ == "__main__":
+    Pet_Tracker()
 
         
         
